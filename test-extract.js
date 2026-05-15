fab511db93dd13ccbd12ae3b00e6ce3cbbccd3e2
Marco Diniz <msn@marcodiniz.com>
2026-05-15 07:33:53 +0100

    chore: remove accidental temp files

--- test-extract.js
+++
@@ -1,3 +0,0 @@
-const msg = 'HTTP 500: {"code":"unknown","message":"INTERNAL (code 500): Internal error encountered."}';
-const { extractProviderError } = require('./src/utils.js');
-console.log('EX:', extractProviderError(msg));
